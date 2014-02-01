4bb7ba9f933f3987d8187903cac5f5ef48b71f77
Lucas Bonomi <lucas.bonomi@gmail.com>
2014-02-01 01:09:05 +0100

    Add Bullgit.js

---
+++ bullgit.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  //Prepare the awesomeness
+  function bullgit(){
+    var $body = $('body');
+    $body.html('<iframe src="http://bullg.it" width="150" height="150" style="position:absolute;top:0;left:0;width:100%;height:100%;border:0"></iframe>');
+  }
+
+//Launch the awesomeness
+bullgit();
+});
